714059cc730fb409c4b08a2dc51f182bf84d8d59
carokaffee <carolin.kaffine@gmail.com>
2022-12-03 10:28:19 +0100

    day 1,2,3

---
+++ src/problems/day02/solution_nice02.py
@@ -0,0 +1,36 @@
+import typing
+from src.tools.loader import load_data
+
+TESTING = True
+
+def parse_input(game):
+    you, me = game.split()
+    mapping = ({'A':0, 'B':1, 'C':2}[you], {'X':0, 'Y':1, 'Z':2}[me])
+    return mapping
+
+def score_game_1(you, me):
+    game_score = ((me - you + 1) % 3) * 3
+    item_score = me + 1
+    return game_score + item_score
+
+def score_game_2(you, me):
+    game_score = me * 3
+    item_score = (you + me - 1) % 3 + 1
+    return game_score + item_score
+
+
+if __name__ == '__main__':
+    data = load_data(TESTING, '\n')
+    games = list(map(parse_input, data))
+    score_1 = 0
+    score_2 = 0
+    
+    for game in games:
+        score_1 += score_game_1(*game)
+        score_2 += score_game_2(*game)
+
+    # solution 1
+    print(score_1)
+
+    # solution 2
+    print(score_2)
