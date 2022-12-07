a455942bba120670e53e78e66205bf66cfea7797
Carolin Kaffine <carolin.kaffine@wetteronline.de>
2022-12-07 16:52:25 +0100

    day 7

--- src/problems/day07/solution07.py
+++ src/problems/day07/solution07.py
@@ -2,5 +2,90 @@ from src.tools.loader import load_data
 
 TESTING = True
 
+def execute_command(command):
+    if command == '$ cd /':
+        go_to_root()
+    elif command == '$ cd ..':
+        go_back()
+    elif command.startswith('$ cd '):
+        enter_dir(command[5:])
+    elif command.startswith('dir '):
+        create_dir(command[4:])
+    elif command == '$ ls':
+        pass
+    else:
+        add_file(*command.split())
+    
+def go_to_root():
+    global current_dir
+    current_dir = ['']
+
+def go_back():
+    current_dir.pop()
+
+def enter_dir(dir):
+    current_dir.append(dir)
+
+def create_dir(name):
+    pwd = '/'.join(current_dir)
+    new_dir = pwd + '/' + name
+    existing_dirs.append(new_dir)
+    files_in_dir[new_dir] = []
+    files_in_dir[pwd].append((f'dir::{name}', 0))
+
+def add_file(size, name):
+    pwd = '/'.join(current_dir)
+    files_in_dir[pwd].append((f'file::{name}', int(size)))
+    
+
+def sort_by_depth(dirs):
+    return list(reversed(sorted(dirs, key=lambda a: a.count('/'))))
+
+def calculate_dir_size(dirs):
+    sizes = {}
+    for dir in dirs:
+        sizes[dir] = 0
+        for name, size in files_in_dir[dir]:
+            sizes[dir] += size
+            if name[:5] == 'dir::':
+                sizes[dir] += sizes[dir + '/' + name[5:]]
+    return sizes
+
+def sum_of_small_dirs(dirs):
+    size = 0
+    for val in dirs.values():
+        if val <= 100000:
+            size += val
+    return size
+
+def size_to_free_up_space(dirs):
+    large_dirs = []
+
+    for key, val in dirs.items():
+        if val >= 30000000 - (70000000 - dirs['']):
+            large_dirs.append((val, key))
+    return sorted(large_dirs)[0][0]
+
+
 if __name__ == '__main__':
     data = load_data(TESTING, '\n')
+
+    current_dir = ['']
+    existing_dirs = ['']
+    files_in_dir = {'' : []}
+
+    for command in data:
+        execute_command(command)
+
+    dirs_sorted_by_depth = sort_by_depth(existing_dirs)
+    dir_size = calculate_dir_size(dirs_sorted_by_depth)
+
+
+    solution_1 = sum_of_small_dirs(dir_size)
+    solution_2 = size_to_free_up_space(dir_size)
+    
+    # solution 1
+    print(solution_1)
+
+    # solution 2
+    print(solution_2)
