c4e0147e1e0d08a90ba746309befceb196343854
carokaffee <carolin.kaffine@gmail.com>
2022-12-13 09:30:35 +0100

    day 13

--- src/problems/day13/solution13.py
+++ src/problems/day13/solution13.py
@@ -1,4 +1,5 @@
 from src.tools.loader import load_data
+from more_itertools import chunked
 
 TESTING = True
 
@@ -7,42 +8,29 @@ def parse_input(data):
     result = []
     for pair in data:
         a, b = pair.split('\n')
+        for p in a + b:
+            assert p in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '[', ']', ',', ' ']
         result.append(eval(a))
         result.append(eval(b))
     return result
 
 
-def compare_integers(a,b):
-    if a < b:
-        return True
-    elif a > b:
-        return False
-    else:
-        return 'continue'
+def compare_integers(a, b):
+    if a != b:
+        return a < b
 
 
-def compare_lists(a,b):
-    length = min(len(a), len(b))
-    if len(a) == 0 and len(b) == 0:
-        return 'continue'
-    elif len(b) == 0:
-        return False
-    elif len(a) == 0:
-        return True
-    for i in range(length):
-        if compare(a[i], b[i]) == True:
-            return True
-        elif compare(a[i], b[i]) == False:
-            return False
-    if len(a) > len(b):
-        return False
-    elif len(a) < len(b):
-        return True
-    else:
-        return 'continue'
+def compare_lists(a, b):
+    for el1, el2 in zip(a, b):
+        comparison = is_in_right_order(el1, el2)
+        if comparison is not None:
+            return comparison
+    
+    if len(a) != len(b):
+        return len(a) < len(b)
 
 
-def compare(a,b):
+def is_in_right_order(a, b):
     if type(a) == int and type(b) == int:
         return compare_integers(a,b)
     elif type(a) == int and type(b) == list:
@@ -53,33 +41,39 @@ def compare(a,b):
         return compare_lists(a,b)
 
 
+def sum_of_ordered_pair_indices(signals):
+    correct_indices = []
+
+    for i, (a, b) in enumerate(chunked(signals, 2)):
+        if is_in_right_order(a, b):
+            correct_indices.append(i + 1)
+        
+    return sum(correct_indices)
+
+
+def find_decoder_key(signals):
+    signals.append([[2]])
+    signals.append([[6]])
+
+    # Bubble sort! oOoOo8o8Oo
+    for _ in range(len(signals)):
+        for i in range(len(signals) - 1):
+            if not is_in_right_order(signals[i], signals[i + 1]):
+                signals[i], signals[i + 1] = signals[i + 1], signals[i]
+    
+    a = signals.index([[2]]) + 1
+    b = signals.index([[6]]) + 1
+
+    return a * b
+
+
 
 if __name__ == '__main__':
     data = load_data(TESTING, '\n\n')
-    signal = parse_input(data)
-    correct_indices = []
-
-    for i in range(len(signal) // 2):
-        if compare(signal[2 * i], signal[2 * i + 1]):
-            correct_indices.append(i+1)
-        
-    print(sum(correct_indices))
-
-    signal2 = parse_input(data)
-    signal2.append([[2]])
-    signal2.append([[6]])
-
-    for _ in range(1000):
-        for i, signal in enumerate(signal2[:-1]):
-            if not compare(signal2[i], signal2[i+1]):
-                signal2[i], signal2[i+1] = signal2[i+1], signal2[i]
-
-    a = 0
-    b = 0
-    for i, signals in enumerate(signal2):
-        if signals == [[2]]:
-            a = i+1
-        if signals == [[6]]:
-            b = i+1
+    signals = parse_input(data)
     
-    print(a,b,a*b)
+    # part 1
+    print(sum_of_ordered_pair_indices(signals))
+
+    # part 2
+    print(find_decoder_key(signals))
