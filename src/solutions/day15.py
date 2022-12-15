048dc2b86ef87405786aac808e192f7a88ed6978
Carolin Kaffine <carolin.kaffine@wetteronline.de>
2022-12-15 07:47:55 +0100

    working day 15 part 2

--- src/solutions/day15.py
+++ src/solutions/day15.py
@@ -1,20 +1,21 @@
 from src.tools.loader import load_data
+from tqdm import tqdm
 
 
 TESTING = False
 
 
 def parse_input(data):
-    closest_beacon = {}
+    sensors_beacons = {}
     for line in data:
         words = line.split()
         sensor_x = int(words[2][2:-1])
         sensor_y = int(words[3][2:-1])
         beacon_x = int(words[-2][2:-1])
         beacon_y = int(words[-1][2:])
-        closest_beacon[(sensor_x, sensor_y)] = (beacon_x, beacon_y)
+        sensors_beacons[(sensor_x, sensor_y)] = (beacon_x, beacon_y)
 
-    return closest_beacon
+    return sensors_beacons
 
 
 def manhattan_distance(p1, p2):
@@ -28,33 +29,34 @@ def is_within_distance(p1, p2, dist):
 
 if __name__ == "__main__":
     data = load_data(TESTING, "\n")
-    closest_beacon = parse_input(data)
+    sensors_beacons = parse_input(data)
     dist_to_closest_beacon = {
         sensor: manhattan_distance(sensor, beacon)
-        for sensor, beacon in closest_beacon.items()
+        for sensor, beacon in sensors_beacons.items()
     }
     # print(closest_beacon)
     # print(dist_to_closest_beacon)
 
     min_x = min(
-        [x for x, y in set(closest_beacon.keys()).union(closest_beacon.values())]
+        [x for x, y in set(sensors_beacons.keys()).union(sensors_beacons.values())]
     )
     min_y = min(
-        [y for x, y in set(closest_beacon.keys()).union(closest_beacon.values())]
+        [y for x, y in set(sensors_beacons.keys()).union(sensors_beacons.values())]
     )
     max_x = max(
-        [x for x, y in set(closest_beacon.keys()).union(closest_beacon.values())]
+        [x for x, y in set(sensors_beacons.keys()).union(sensors_beacons.values())]
     )
     max_y = max(
-        [y for x, y in set(closest_beacon.keys()).union(closest_beacon.values())]
+        [y for x, y in set(sensors_beacons.keys()).union(sensors_beacons.values())]
     )
     max_dist = max([y for y in dist_to_closest_beacon.values()])
     print(min_x, min_y, max_x, max_y)
 
-    Y_ROW = 2000000
-    occupied = set(closest_beacon.values())
+    Y_ROW = 20
+    occupied = set(sensors_beacons.values())
 
     counter = 0
+    """
     for x in range(min_x - max_dist, max_x + 1 + max_dist):
         is_in_distance = False
         for sensor, dist in dist_to_closest_beacon.items():
@@ -68,5 +70,40 @@ if __name__ == "__main__":
             # print(x)
             counter += 1
             # print()
+    """
 
-    print(counter)
+    DIM = 4000000
+
+    possible_points = set()
+    for sensor, dist in tqdm(dist_to_closest_beacon.items()):
+        sens_x, sens_y = sensor
+        for x in range(dist + 2):
+            y = dist + 1 - x
+            possible_points.update(
+                set(
+                    (
+                        (sens_x + x, sens_y + y),
+                        (sens_x - x, sens_y + y),
+                        (sens_x + x, sens_y - y),
+                        (sens_x - x, sens_y - y),
+                    )
+                )
+            )
+    print(len(possible_points))
+
+    real_possible_points = set()
+    for x, y in tqdm(possible_points):
+        if x >= 0 and x <= DIM and y >= 0 and y <= DIM:
+            real_possible_points.add((x, y))
+
+    print(len(real_possible_points))
+
+    for x, y in tqdm(real_possible_points):
+        is_in_distance = False
+        for sensor, dist in dist_to_closest_beacon.items():
+            possibility = is_within_distance(sensor, (x, y), dist)
+            if possibility:
+                is_in_distance = True
+
+        if not is_in_distance:
+            print(x, y)
