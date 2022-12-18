813151dc6b903f7bda23e02ad151c3df924e8be4
carokaffee <carolin.kaffine@gmail.com>
2022-12-18 19:41:22 +0100

    day 19 start

--- src/solutions/day19.py
+++ src/solutions/day19.py
@@ -2,5 +2,12 @@ from src.tools.loader import load_data
 
 TESTING = True
 
-if __name__ == '__main__':
-    data = load_data(TESTING, '\n')
+
+def parse_input(data):
+    return
+
+
+if __name__ == "__main__":
+    data = load_data(TESTING, "\n")
+    xyz = parse_input(data)
+    print(data)
